28905c19b302515dcf0c79a28b26889e4da725c6
Abdoulaye DIA <abdoudu78130@gmail.com>
2022-12-08 08:16:42 +0100

    setup bot.py file with an instance of the BotManager

--- bot.py
+++ bot.py
@@ -1,32 +1,11 @@
-# import os
+import os
+import discord
+from discord.ext import commands
 
-# import discord
-# from dotenv import load_dotenv
+from modules.bot_manager import BotManager
+from dotenv import load_dotenv
 
-# from modules import login, data_manager, notification
-# from utils import redis_manager
+load_dotenv()
 
-# load_dotenv()
-
-# client = discord.Client()
-# client = commands.Bot(command_prefix='/')
-
-
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to Discord!')
-
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('!login'):
-#         username = message.content.split()[1]
-#         password = message.content.split()[2]
-#         token = login.get_token(username, password)
-#         redis_manager.set_token(message.author.id, token)
-#         await message.channel.send(f'{message.author.mention} has logged in successfully!')
-
-# client.run(os.getenv('DISCORD_TOKEN'))
+manager = BotManager()
+manager.run()
---
+++ modules/__init__.py
@@ -0,0 +1,4 @@
+from .login import *
+from .data_manager import *
+from .notification import *
+from .bot_manager import *
